71ad61d95c3ab72e2324bc7772dcb85eb7103317
Gabriel Julio <gabrieljuliobs@gmail.com>
2020-04-08 20:07:56 -0300

    Corrigido distancias
    Distancias corrigidas:
    - Manhattan
    - Chebyshev

--- lib/distancia.py
+++ lib/distancia.py
@@ -19,14 +19,14 @@ def euclidiana(class1_center, class2_center, class3_center, data):
         idx = np.argmin(distances)  # retorna o index do menor item
 
         if idx == 0:
-            class1_qtd = class1_qtd + 1
+            class1_qtd += 1
         elif idx == 1:
-            class2_qtd = class2_qtd + 1
+            class2_qtd += 1
         else:
-            class3_qtd = class3_qtd + 1
+            class3_qtd += 1
 
-    print(f'\nQuantidade pela média Euclidiana:\n- Class 1: {class1_qtd}'
-          f'\n- Class 2: {class2_qtd}\n- Class 3: {class3_qtd}')
+    print(f'\nInstâncias pela classificadas pela média Euclidiana:\n- Classe 1: {class1_qtd}'
+          f'\n- Classe 2: {class2_qtd}\n- Classe 3: {class3_qtd}')
 
 
 def manhattan(class1_center, class2_center, class3_center, data):
@@ -40,20 +40,20 @@ def manhattan(class1_center, class2_center, class3_center, data):
         """
         Manhattan
         """
-        distances = np.array([abs((class1_center - data[i][0:4]).sum()),
-                              abs((class2_center - data[i][0:4]).sum()),
-                              abs((class3_center - data[i][0:4]).sum())])
+        distances = np.array([abs((class1_center - data[i][0:4])).sum(),
+                              abs((class2_center - data[i][0:4])).sum(),
+                              abs((class3_center - data[i][0:4])).sum()])
         min_ = np.min(distances)  # retorna o item menor (menor distancia Manhattan)
         idx = np.argmin(distances)  # retorna o index do menor item
-        """
-        print(distances,  'Menor: distancia',min_, 'classe:',idx)
-        """
+
+        # print(distances,  'Menor: distancia',min_, 'classe:',idx)
+
         if idx == 0:
-            class1_qtd = class1_qtd + 1
+            class1_qtd += 1
         elif idx == 1:
-            class2_qtd = class2_qtd + 1
+            class2_qtd += 1
         else:
-            class3_qtd = class3_qtd + 1
+            class3_qtd += 1
 
     print(f'\nQuantidade pela média Manhattan:\n- Class 1: {class1_qtd}'
           f'\n- Class 2: {class2_qtd}\n- Class 3: {class3_qtd}')
@@ -71,20 +71,21 @@ def chebyshev(class1_center, class2_center, class3_center, data):
         """
         Chebyshev
         """
-        distances = np.array([abs((class1_center - data[i][0:4]).sum()),
-                              abs((class2_center - data[i][0:4]).sum()),
-                              abs((class3_center - data[i][0:4]).sum())])
-        max_ = np.max(distances)  # retorna o item maior (menor distancia Chebyshev)
-        idx = np.argmin(distances)  # retorna o index do menor item
-        """
-        print(distances,  'Maior: distancia',max_, 'classe:',idx)
-        """
+        distances = np.array([abs((class1_center - data[i][0:4])).max().sum(),
+                              abs((class2_center - data[i][0:4])).max().sum(),
+                              abs((class3_center - data[i][0:4])).max().sum()])
+
+        max_ = np.max(distances)  # retorna o item maior (maior distancia Chebyshev)
+        idx = np.argmin(distances)  # retorna o index do maior item
+
+        # print(distances,  'Maior: distancia',max_, 'classe:',idx)
+
         if idx == 0:
-            class1_qtd = class1_qtd + 1
+            class1_qtd += 1
         elif idx == 1:
-            class2_qtd = class2_qtd + 1
+            class2_qtd += 1
         else:
-            class3_qtd = class3_qtd + 1
+            class3_qtd += 1
 
     print(f'\nQuantidade pela média Chebyshev:\n- Class 1: {class1_qtd}'
           f'\n- Class 2: {class2_qtd}\n- Class 3: {class3_qtd}')
